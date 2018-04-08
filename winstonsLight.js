8bfb94daf7f7c91ddde5f5a9b5c49310fabc0267
Dan Allen <justdanallen@gmail.com>
2018-04-08 03:14:07 -0600

    Add retries to setBinaryState and periodically poll for binary state

--- winstonsLight.js
+++ winstonsLight.js
@@ -3,34 +3,84 @@ const { fork } = require('child_process')
 const ping = require('ping')
 const async = require('async')
 const config = require('./config')
+const secret = require('./secret')
 const Wemo = require('wemo-client')
 const wemo = new Wemo()
 
 
 let client = null
-const connectedRef = firebase.database().ref('/winstons-light/connected')
-const deviceInfoRef = firebase.database().ref('/winstons-light/deviceInfo')
+const db = firebase.database()
+const connectedRef = db.ref('/winstons-light/connected')
+const deviceInfoRef = db.ref('/winstons-light/deviceInfo')
+const errorRef = db.ref(secret.firebase.errorRef)
 
+let binaryState = null
+let command = null
+let setBinaryStateTries = 0
 
 exports.on = () => {
-    if(client) {
-      client.setBinaryState(1)
-    }
-    else {
-      log('no device found yet')
-    }
-  }
+  const one = 1
+  command = one.toString()
+  setBinaryState('1')
+}
 
 exports.off = () => {
-  if(client) {
-    client.setBinaryState(0)
-  }
-  else {
-      log('no device found yet')
+  const zero = 0
+  command = zero.toString()
+  setBinaryState('0')
+}
+
+/**
+ * Sets the binary state of the wemo
+ * @param {String} state 1 is on 0 is off
+ */
+function setBinaryState(state){
+  if(client){
+    // only change if the new state doesn't match the current state
+    log('set state: ' + state + ' current binaryState: ' + binaryState)
+    
+    if(state !== binaryState){
+
+      if(setBinaryStateTries < config.SET_STATE_MAX_RETRIES){
+        logSetBinaryStateTry(state)
+        client.setBinaryState(state, setBinaryStateCallback)
+      } 
+
+    }
+  } else {
+    errorHandler('notFound', {message: 'tried to set state before device is found'})
+    log('no device found yet')
   }
 }
 
 
+function setBinaryStateCallback(err, state) {
+  if(err){
+    errorHandler('setBinaryState', err)
+  }
+
+  binaryState = state.BinaryState
+  if(setBinaryStateTries > SET_STATE_NUM_RETRIES){
+    getBinaryState()
+  }
+
+  // retry when the return state doesn't match the command
+  if(state.BinaryState !== command) {
+    setTimeout( ()=>{
+      setBinaryState(command)
+    }, SET_STATE_TIME_BETWEEN) 
+
+  } else {
+    setBinaryStateTries = 0
+  }
+}
+
+function logSetBinaryStateTry(state){
+  setBinaryStateTries++
+  log('set binary state try: SET: ' + command + 
+      ' CURRENT: ' + binaryState +
+      ' TRIES: ' + setBinaryStateTries)
+}
 
 /**
  * Discover the wemo and set the client
@@ -83,17 +133,18 @@ function updateClient(deviceInfo){
     deviceInfoRef.set(deviceInfo)
   
     client.on('binaryState', value => {
+      binaryState = value
       log('Binary State changed to: ' + value);
     })
     
     client.on('error', err => {
-      log(err)
+      errorHandler('wemoClient', err)
     })
   }
   else{
     log('Setting wemo client: null')
     connectedRef.set(isNotConnectedMessage())
-    deviceInfoRef = null
+    deviceInfoRef.set(null)
     client = null
   }
 
@@ -126,8 +177,21 @@ function discoveryLoop(numTries) {
   return new Promise((resolve, reject) => {
     async.retry(numTries, singleDiscovery, (error, result) => {
       if(error) {
+        updateClient(null)
         reject(error)
+
       } else {
+        // now that we found it, set the state to match FB
+        console.log('found: ' + result.binaryState)
+        binaryState = result.binaryState
+
+        if(result.binaryState !== command){
+          setBinaryState(command)
+        }
+
+        // set the wemo client
+        updateClient(result)
+        
         resolve(result)
       }
     })
@@ -143,7 +207,7 @@ function discovery(){
   .then(function(deviceInfo){
 
     // start a ping loop keep going until device doesn't respond
-    updateClient(deviceInfo)
+
 
     // // monitor client until lost
     let pingLoop = setInterval(function() { 
@@ -176,8 +240,30 @@ setInterval( () => {
 }, config.DISCOVERY_RETRY_TIME_INTERVAL)
 
 
+/**
+ * Gets the binary state and addes it to the local variable
+ * binaryState
+ */
+function getBinaryState(){
+  if(client){
+    client.getBinaryState( (err, state)=>{
+      if(!err){
+        // returns string: '1' or '0'
+        binaryState = state
+        log('Got current binary state: ' + binaryState)
+        deviceInfoRef.update({binaryState: state})
+      }
+    })
+  }
+}
+
+// poll for the current state
+setInterval( () => {
+  getBinaryState()
+}, config.SYNC_BINARYSTATE_INTERVAL)
+
 /** 
- * Pings the client until no response. When now response, it resets the client
+ * Pings the client until no response. When no response, it resets the client
  * so that discover will start.
 */
 function pinging(){
@@ -195,20 +281,21 @@ function pinging(){
         //try one more ping
         setTimeout(() => {
 
-            ping.sys.probe(ipAddress, function(secondIsAlive){
 
-              log(`retry pinging: ${ipAddress}  result: ${isAlive}`)
-              
-              if(!secondIsAlive){
-                // it's really dead!
-                log('super dead')
+          ping.sys.probe(ipAddress, function(secondIsAlive){
 
-              } else {
-                // bring it back right away!
-                discovery()
-              }
+            log(`retry pinging: ${ipAddress}  result: ${isAlive}`)
             
-            })
+            if(!secondIsAlive){
+              // it's really dead!
+              log('super dead')
+
+            } else {
+              // bring it back right away!
+              discovery()
+            }
+          
+          })
 
         }, config.PING_TIME_INTERVAL)
       } else {
@@ -216,4 +303,19 @@ function pinging(){
         connectedRef.set(isConnectedMessage())
       }
     })
+}
+
+/**
+ * Pass errors up to firebase for debugging
+ * @param {String} type Error type for firebase
+ * @param {Object} err The error objec
+ */
+function errorHandler(type, err){
+  let obj = {}
+  obj[type] = {
+    time: new Date().toString(),
+    error: err.message
+  }
+  errorRef.update(obj)
+  log('Error: ' + type + ' - ' + err.message)
 }
